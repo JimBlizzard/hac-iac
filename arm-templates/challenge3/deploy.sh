81a77652dd8f2627f99bdd365ce96009679ac57b
Jim Blizzard <jimblizz@hotmail.com>
2020-09-22 14:05:33 -0400

    minor modifications

--- arm-templates/challenge3/deploy.sh
+++ arm-templates/challenge3/deploy.sh
@@ -4,4 +4,4 @@ az login
 
 az group create -n blizz-arm-vscode -l eastus2
 
-az deployment group create -g blizz-arm-vscode --template-file azuredeploy.json --parameters azuredeploy.parameters.json
+az deployment group create -g blizz-arm-vscode --template-file template.json --parameters parameters.json
